0768038b1fac9ed51a578b6a58292379210c075c
marthajenny112 <marthajenny@protonmail.com>
2026-02-25 23:27:14 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -29,7 +29,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 
 html_theme = 'alabaster'
-html_title = "Guide to Install VIPRE with Product Key Code that Protects and Secure"
+html_title = "Guide to Install VVIPAR with Product Key Code that Protects and Secure"
 
 # You can still add html_meta here if you want, but with the new Read the Docs addons
 # custom template is the reliable way to inject meta tags.
